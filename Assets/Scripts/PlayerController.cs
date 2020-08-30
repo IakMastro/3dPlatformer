8863f1e3d2572db0083bbc512653f4c4bbc126f7
IakMastro <cse242017102@uniwa.gr>
2020-08-30 21:02:54 +0300

    Part 2: Simple Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [FormerlySerializedAs("MoveSpeed")] public float moveSpeed;
+    [FormerlySerializedAs("theRB")] public Rigidbody theRb;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 
+            theRb.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRb.velocity = new Vector3(theRb.velocity.x, jumpForce, theRb.velocity.z);
+        }
+    }
+}
